880e52b875361b0b0e56d58b4b54cc09df29ed29
Talha2000 <amjad_maqsood90@hotmail.com>
2023-04-28 16:38:44 -0400

    Update Register.jsx

--- stock-tracker/src/pages/Register.jsx
+++ stock-tracker/src/pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () =>  {
   const handleSubmit = async e => {
     e.preventDefault()
     try {
-      await axios.post("/auth/register", inputs);
+      await axios.post("/api/auth/register", inputs);
       navigate("/login");
     }
     catch (err) {
